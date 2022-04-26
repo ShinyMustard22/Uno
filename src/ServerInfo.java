c8f66040345e2efd327efaf95e0a5199a00a6c2c
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-04-25 18:43:29 -0700

    Created reading input and output functions.

--- src/ServerInfo.java
+++ src/ServerInfo.java
@@ -1,3 +1,28 @@
+import java.io.*;
+
 public class ServerInfo {
     public static final int PORT_NUM = 9001;
+
+    public static String readInput(DataInputStream in) {
+        String input;
+
+        try {
+            input = in.readUTF();
+            while(input.isEmpty()) {
+                input = in.readUTF();
+            }
+        } catch (IOException ex) {
+            return null;
+        }
+
+        return input;
+    }
+
+    public static void writeOutput(DataOutputStream out, String output) {
+        try{
+            out.writeUTF(output);
+        } catch (IOException ex) {
+            System.exit(1);
+        }
+    }
 }
