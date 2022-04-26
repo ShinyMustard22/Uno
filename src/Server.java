19668a2e5e10ada4874a7225250c15bfdb3cb4fa
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-04-25 18:45:25 -0700

    Made server able to accept an input when connected to.

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,32 @@
+import java.io.*;
+import java.net.*;
+
 public class Server {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String input;
+
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(ServerInfo.PORT_NUM);
+            socket = serverSocket.accept();
+            in = new DataInputStream(
+                new BufferedInputStream(socket.getInputStream()));
+            out = new DataOutputStream(
+                new BufferedOutputStream(socket.getOutputStream()));
+
+            while (true) {
+                input = ServerInfo.readInput(in);
+                // Do something with the input
+            }
+        } catch (IOException ex) {
+            
+        }
+    }
+
     public static void main(String[] args) {
-        
+        new Server();
     }
 }
