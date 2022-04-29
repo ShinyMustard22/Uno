db1ef591031df2ede011a89895c4179ad6790459
Srushti C <schaudhari323@student.fuhsd.org>
2022-04-28 23:36:18 -0700

    made frame and added icon

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.URL;
+import java.awt.*;
+
 import javax.swing.*;
+import javax.tools.Tool;
 
 public class Main extends JFrame {
+
+    private static final int width = 800;
+    private static final int height = 600;
 
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
 
     public Main() {
+        super("Uno"); 
+        setBounds(0, 0, width, height);
+        Image icon = Toolkit.getDefaultToolkit().getImage("Uno\\src\\unologo.png");  
+        setIconImage(icon);
+
         try {
             socket = new Socket(Server.IP_ADDRESS, Server.PORT_NUM);
             in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
@@ -17,7 +30,11 @@ public class Main extends JFrame {
             killEverything(socket, in, out);
         }
 
-        // Start code here
+             
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false); 
+        setVisible(true); 
     }
 
     private void killEverything(Socket socket, DataInputStream in, DataOutputStream out) {
@@ -39,7 +56,6 @@ public class Main extends JFrame {
     }
 
     public static void main(String[] args) {
-        GameBoard g = new GameBoard();
         new Main();
     }
 }
