2af1f074b456c7536a7ae987d254de33511fbc31
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-04-29 18:34:19 -0700

    changed DeckOfCards to just Deck, and it now it extends arraylist, also got rid of display method since we dont need it

--- src/Deck.java
+++ src/Deck.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
+import cards.*;
 
-public class DeckOfCards
-{
-    private ArrayList<Card> deck = new ArrayList<Card>();
-    public DeckOfCards(){
+public class Deck extends ArrayList<Card> {
+    
+    public Deck(){
         for (int i = 1; i <= 9; i ++ ){
             for (int a = 0; a < 3; a ++ ){
                 for (int r = 0; r < 2; r ++ ){
                     NumberCard card = new NumberCard(i, a);
-                    deck.add(card);
+                    add(card);
                 }
                 
             }
@@ -17,26 +17,26 @@ public class DeckOfCards
         for (int i = 0; i < 3; i ++ ){
             for (int a = 0; a < 2; a ++ ){
                 NumberCard zeroCard = new NumberCard(0, i);
-                deck.add(zeroCard);
+                add(zeroCard);
             }
         }
 
         for (int i = 0; i < 3; i ++ ){
             for (int a = 0; a < 2; a ++ ){
                 ReverseCard revCard = new ReverseCard(i);
-                StopCard stopCard = new StopCard(i);
-                PlusCard p2card = new PlusCard(i, 2);
-                deck.add(revCard); 
-                deck.add(stopCard);
-                deck.add(p2card);
+                SkipCard stopCard = new SkipCard(i);
+                PlusTwoCard p2card = new PlusTwoCard(i, 2);
+                add(revCard); 
+                add(stopCard);
+                add(p2card);
             }
         }
 
         for (int i = 0; i < 3; i ++ ){
             WildCard wild = new WildCard();
-            PlusCard p4card = new PlusCard(null, 2);
-            deck.add(wild);
-            deck.add(p4card);
+            PlusTwoCard p4card = new PlusTwoCard(null, 2);
+            add(wild);
+            add(p4card);
         }
     }
 
@@ -50,15 +50,9 @@ public class DeckOfCards
         for (int a = 0; a < 108/2; a ++){
             int ind1 = (int)(Math.random()*108);
             int ind2 = (int)(Math.random()*108);
-            Card c1 = deck.get(ind1);
-            deck.set(ind1, deck.get(ind2));
-            deck.set(ind2, c1);
-        }
-    }
-
-    public void display(){
-        for (Card c : deck){
-            System.out.print(c.toString() + " ");
+            Card c1 = get(ind1);
+            set(ind1, get(ind2));
+            set(ind2, c1);
         }
     }
 }
