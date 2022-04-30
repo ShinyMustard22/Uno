90f2630503f8fdbbd07e8ca792a51294ea01f460
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-04-30 00:22:05 -0700

    made a list of discarded cards

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,11 +1,15 @@
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import cards.Card;
 
 public class GameBoard {
     
     private Queue<Card> deck;
+    private List<Card> discardPile;
 
     public GameBoard() {
         deck = new Deck();
+        discardPile = new LinkedList<Card>();
     }
 }
