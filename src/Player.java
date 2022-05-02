535bec70a9624bd0b99a984e419ab08c87711a14
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-05-01 17:32:27 -0700

    creating constructor

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,15 @@
+import java.util.LinkedList;
+import java.util.List;
+import cards.Card;
+
 public class Player {
+
+    private String username;
+    private List<Card> hand;
+
+    public Player(String username, List<Card> hand) {
+        this.username = username;
+        this.hand = hand;
+    }
 
 }
