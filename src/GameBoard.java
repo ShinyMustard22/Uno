54ce6a661ed12f6aef5c92129ac7088ed3cf3b89
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-05-01 17:32:37 -0700

    added basic methods

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -5,9 +5,30 @@ public class GameBoard {
     
     private Queue<Card> deck;
     private Stack<Card> discardPile;
+    private ArrayList<Player> players;
+    private boolean gameStarted;
 
     public GameBoard() {
         deck = new Deck();
         discardPile = new Stack<Card>();
+        players = new ArrayList<Player>();
+        gameStarted = false;
+    }
+
+    public void addPlayer(String username) {
+        if (gameStarted) {
+            return;
+        }
+        
+        List<Card> hand = new LinkedList<Card>();
+        for (int count = 0; count < 7; count++) {
+            hand.add(deck.poll());
+        }
+
+        players.add(new Player(username, hand));
+
+        if (players.size() >= 10) {
+            gameStarted = true;
+        }
     }
 }
