d2c5ab8c854b29b478792d86d480b448bc4c4b87
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-05-11 09:40:54 -0700

    stuff

--- src/Server.java
+++ src/Server.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 
 public class Server {
     public static final int PORT_NUM = 6969;
-    public static final String IP_ADDRESS = "localhost";
+    public static final String IP_ADDRESS = "10.18.94.21";
 
     public static final String NAME = "username: ";
     public static final String INIT_PLAYER_LIST = "playerList: ";
