bdd2462048f21683fa71997feebcf94dde45ebe0
Angela <angie.eevee@gmail.com>
2022-05-20 22:56:21 -0700

    added play animation - may do passing cards

--- src/GUIHandler.java
+++ src/GUIHandler.java
@@ -25,8 +25,8 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
     private JMenuBar menuBar;
     private JMenu help;
     private JMenuItem rules;
-    private JMenuItem options; 
-    private JButton soundIcon; 
+    private JMenuItem options;
+    private JButton soundIcon;
     private JTextField nameField;
     private JTextArea errorMessage;
     private JLabel invalidName, enterNamePrompt, waiting;
@@ -34,31 +34,32 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
     private JButton deck;
     private JLabel discardPile;
 
-    private JPanel mainPanel, playerInfo, board, playerHand;
+    private JPanel mainPanel, playerInfo, playerHand;
+    private AnimationPanel board;
     private GridBagConstraints gbc;
     private JTable playerTable;
     private LinkedList<JButton> hand;
     private LinkedList<String> strHand;
 
-    private JDialog optionWindow; 
+    private JDialog optionWindow;
 
-    private JLayeredPane unoLayers; 
-    private JPanel unoPanel; 
+    private JLayeredPane unoLayers;
+    private JPanel unoPanel;
     private JButton unoButton;
     private JButton red, blue, green, yellow, cancelWild;
 
     private JLabel congratulations, spectateLabel;
 
-    private static boolean soundOn = true; 
-    private static final String cardFlippedSound = "cardFlipping"; 
+    private static boolean soundOn = true;
+    private static final String cardFlippedSound = "cardFlipping";
     private static final String playerJoinsOrLeaves = "playerInOrOut";
-    private static final String unoSound = "unoVerbal"; 
-    private static final String errorSound = "wrong"; 
-    public static final String gameStartedSound = "gameStart"; 
+    private static final String unoSound = "unoVerbal";
+    private static final String errorSound = "wrong";
+    public static final String gameStartedSound = "gameStart";
 
     private LinkedHashMap<String, Integer> players;
     private StringBuffer rulesString;
-    
+
     private DataOutputStream out;
     private String myName;
 
@@ -73,7 +74,7 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
 
         setIconImage(unoLogo.getImage());
 
-        optionWindow = new JDialog(this); 
+        optionWindow = new JDialog(this);
         optionWindow.setLayout(new FlowLayout());
         optionWindow.setResizable(false);
         optionWindow.setBounds(0, 0, startingWidth, startingHeight);
@@ -83,12 +84,12 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
         ImageIcon soundImageIcon = new ImageIcon(getClass().getResource("/assets/images/soundOnIcon.png"));
         soundIcon = new JButton(soundImageIcon);
         soundIcon.setSize(soundImageIcon.getIconWidth(), soundImageIcon.getIconHeight());
-        soundIcon.addActionListener(this); 
-        optionWindow.add(soundIcon); 
+        soundIcon.addActionListener(this);
+        optionWindow.add(soundIcon);
 
         mainPanel = new JPanel(new BorderLayout());
 
-        board = new JPanel(new GridBagLayout());
+        board = new AnimationPanel(new GridBagLayout());
         gbc = new GridBagConstraints();
         gbc.insets = new Insets(margin, margin, margin, margin);
 
@@ -116,27 +117,27 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
         // unoLayers = new JLayeredPane();
         // unoLayers = getLayeredPane();
 
-        // UnoLayoutManager unoLayout = new UnoLayoutManager(); 
+        // UnoLayoutManager unoLayout = new UnoLayoutManager();
         // unoLayers.setLayout(unoLayout);
 
-        // unoPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 5)); 
+        // unoPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 5));
         // unoPanel.setBackground(Color.CYAN);
         // // unoPanel.setPreferredSize(new DimensionUIResource(15, 15));
         // // unoLayers.setBounds(0, 0, startingWidth + 10, startingHeight + 10);
 
         // unoLayers.add(mainPanel, JLayeredPane.DEFAULT_LAYER);
         // unoLayout.setBounds(unoPanel, new Rectangle(5, 5, 30, 30)); //change to 0,0 pos
-        
-        // // Insets insets = unoLayers.getInsets(); 
+
+        // // Insets insets = unoLayers.getInsets();
         // // unoPanel.setBounds(insets.left + 2, insets.top + 2, unoPanel.getPreferredSize().width, unoPanel.getPreferredSize().height);
         // // unoPanel.setVisible(false);
 
-        // // add(unoLayers); 
+        // // add(unoLayers);
         // pack();
         // setVisible(true);
-    
+
         add(mainPanel);
-    
+
         players = new LinkedHashMap<String, Integer>();
 
         setVisible(true);
@@ -169,8 +170,8 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
 
         help.add(rules);
 
-        options = new JMenuItem("Options"); 
-        options.addActionListener(this); 
+        options = new JMenuItem("Options");
+        options.addActionListener(this);
         help.add(options);
 
         menuBar.add(help);
@@ -180,7 +181,7 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
         waiting = new JLabel("Waiting for the game to start...");
         waiting.setFont(new Font(waiting.getFont().getName(), Font.PLAIN, 32));
         board.add(waiting, gbc);
-        
+
         board.revalidate();
         board.repaint();
 
@@ -212,7 +213,7 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
             }
             index++;
         }
-        
+
         playerTable = new JTable(data, columnNames);
         playerTable.setPreferredScrollableViewportSize(new Dimension(getWidth() - 10 * 2, playerTable.getMinimumSize().height));
         playerTable.setFillsViewportHeight(true);
@@ -260,10 +261,10 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
 
         ImageIcon faceDown = new ImageIcon(getClass().getResource("/assets/images/card_face_down.png"));
         deck = new JButton(faceDown);
-        board.add(deck, gbc);  
+        board.add(deck, gbc);
         deck.setSize(faceDown.getIconWidth(), faceDown.getIconHeight());
         deck.addActionListener(this);
-        
+
         ImageIcon lastCard = new ImageIcon(getClass().getResource("/assets/images/" + firstCard + ".png"));
         discardPile = new JLabel(lastCard);
         board.add(discardPile, gbc);
@@ -285,11 +286,11 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
 
         board.revalidate();
         board.repaint();
-        
+
         revalidate();
-        repaint(); 
+        repaint();
     }
-    
+
     private void createHand(java.util.List<JButton> newCards) {
         for (JButton card : newCards) {
             playerHand.add(card);
@@ -302,7 +303,7 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
 
         revalidate();
         repaint();
-    } 
+    }
 
     private void updateDiscardPile(String card) {
         discardPile.setIcon(new ImageIcon(getClass().getResource("/assets/images/" + card + ".png")));
@@ -317,16 +318,18 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
 
     private void removeCard(String card) {
         int i = strHand.indexOf(card);
-        playerHand.remove(hand.get(i)); 
+        int x = hand.get(i).getX();
+        playerHand.remove(hand.get(i));
 
-        strHand.remove(i); 
+        strHand.remove(i);
         hand.remove(i);
 
         playerHand.revalidate();
         playerHand.repaint();
 
         revalidate();
-        repaint(); 
+        repaint();
+        board.moveCard(card, x, board.getHeight(), discardPile.getX(), discardPile.getY());
     }
 
     private void chooseColorScreen() {
@@ -339,7 +342,7 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
         try {
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
         } catch(Exception e){
-           e.printStackTrace(); 
+           e.printStackTrace();
         }
 
         red = new JButton("Red");
@@ -397,7 +400,7 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
         try{
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch(Exception e){
-           e.printStackTrace(); 
+           e.printStackTrace();
         }
 
         board.revalidate();
@@ -455,16 +458,16 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
     }
 
     private void spawnUno() {
-        int x = (int)(Math.random() * getWidth()); 
+        int x = (int)(Math.random() * getWidth());
         int y = (int)(Math.random() * getHeight());
-        
+
         unoButton = new JButton("UNO!");
         unoButton.setSize(10,10);
 
         // unoPanel.add(unoButton);
         // unoPanel.setLocation(x, y);
         // unoPanel.setVisible(true);
-        
+
         // unoPanel.revalidate();
         // unoPanel.repaint();
 
@@ -476,14 +479,14 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
     }
     private void toggleSound() {
         if (soundOn) {
-            soundOn = false; 
+            soundOn = false;
             ImageIcon soundImageIcon = new ImageIcon(getClass().getResource("/assets/images/soundOffIcon.png"));
             soundIcon.setIcon(soundImageIcon);
             soundIcon.setSize(soundImageIcon.getIconWidth(), soundImageIcon.getIconHeight());
-        } 
-        
+        }
+
         else {
-            soundOn = true; 
+            soundOn = true;
             ImageIcon soundImageIcon = new ImageIcon(getClass().getResource("/assets/images/soundOnIcon.png"));
             soundIcon.setIcon(soundImageIcon);
             soundIcon.setSize(soundImageIcon.getIconWidth(), soundImageIcon.getIconHeight());
@@ -491,26 +494,26 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
     }
 
     public static void playSound(String soundType) {
-        if (soundOn) { 
+        if (soundOn) {
             File soundFile;
-            AudioInputStream input; 
-            Clip sound; 
-        
+            AudioInputStream input;
+            Clip sound;
+
             try {
-                soundFile = new File ("src/assets/audio/"+ soundType +".wav"); 
+                soundFile = new File ("src/assets/audio/"+ soundType +".wav");
                 try {
                     input = AudioSystem.getAudioInputStream(soundFile.getAbsoluteFile());
-                    sound = AudioSystem.getClip(); 
+                    sound = AudioSystem.getClip();
                     sound.open(input);
                     sound.start();
-        
+
                 } catch(Exception e) {
-                    
+
                 }
             } catch (Exception e) {
                 System.out.println("audio file not found");
             }
-        }   
+        }
     }
 
     public void decode(String allStrData) {
@@ -527,22 +530,22 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
                         }
                         waitingScreen();
                     }
-            
+
                     else if (strData.contains(Server.ADD_PLAYER)) {
                         players.put(strData.substring(Server.ADD_PLAYER.length()), Player.STARTING_HAND_SIZE);
                         playSound(playerJoinsOrLeaves);
                         updateTable();
                     }
-    
+
                     else if (strData.contains(Server.REMOVE_PLAYER)) {
                         players.remove(strData.substring(Server.REMOVE_PLAYER.length()));
                         updateTable();
                     }
-    
+
                     else if (strData.contains(Server.INVALID_USERNAME)) {
                         invalidName.setText("This username contains illegal characters...");
                     }
-    
+
                     else if (strData.contains(Server.TAKEN_USERNAME)) {
                         invalidName.setText("This username has been taken...");
                     }
@@ -550,7 +553,7 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
                     else if (strData.contains(Server.FIRST_CARD)) {
                         createBoard(strData.substring(Server.FIRST_CARD.length()));
                     }
-    
+
                     else if (strData.contains(Server.INIT_PLAYER_HAND)) {
                         hand = new LinkedList<JButton>();
                         strHand = new LinkedList<String>();
@@ -559,13 +562,13 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
                         for (String card : strPlayerHand) {
                             ImageIcon icon = new ImageIcon(getClass().getResource("/assets/images/" + card.toString() + ".png"));
                             newCards.add(new JButton(icon));
-                            strHand.add(card.toString()); 
+                            strHand.add(card.toString());
                         }
 
                         hand.addAll(newCards);
                         createHand(newCards);
                     }
-    
+
                     else if (strData.contains(Server.AM_LEADER)) {
                         if (strData.contains("true")) {
                             makeLeader();
@@ -574,19 +577,19 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
 
                     else if (strData.contains(Server.SET_LEADER)) {
                         makeLeader();
-                    } 
+                    }
 
                     else if (strData.contains(Server.PLAY_CARD)) {
                         String card = strData.substring(Server.PLAY_CARD.length());
                         removeCard(card);
-                    } 
+                    }
 
                     else if (strData.contains(Server.SOMEBODY_PLAYED_CARD)) {
                         String[] card = strData.substring(Server.SOMEBODY_PLAYED_CARD.length()).split(" ");
                         updateDiscardPile(card[1]);
                         players.replace(card[0], players.get(card[0]) - 1);
                         updateTable();
-                    } 
+                    }
 
                     else if (strData.contains(Server.DRAW_CARDS)) {
                         String[] cardsToAdd = strData.substring(Server.DRAW_CARDS.length()).split(" ");
@@ -594,7 +597,7 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
 
                         for (String strCard : cardsToAdd) {
                             playSound(cardFlippedSound);
-                            
+
                             ImageIcon icon = new ImageIcon(getClass().getResource("/assets/images/" + strCard + ".png"));
                             newCards.add(new JButton(icon));
                             strHand.add(strCard);
@@ -646,7 +649,7 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
             myName = nameField.getText();
             write(Server.NAME + nameField.getText());
         }
-        
+
         else if (e.getSource() == startGame) {
             write(Server.GAME_STARTED);
             playSound(gameStartedSound);
@@ -664,7 +667,7 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
 
         else if (e.getSource() == rules) {
              JOptionPane.showMessageDialog(this, rulesString.toString(), "Uno Rules", JOptionPane.PLAIN_MESSAGE);
-        } 
+        }
 
         else if (e.getSource() == options) {
            optionWindow.setVisible(true);
@@ -704,29 +707,29 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
     @Override
     public void componentResized(ComponentEvent e) {
        if (e.getSource() == this) {
-           
+
        }
     }
 
 
     @Override
     public void componentMoved(ComponentEvent e) {
-        
-        
+
+
     }
 
 
     @Override
     public void componentShown(ComponentEvent e) {
-        
-        
+
+
     }
 
 
     @Override
     public void componentHidden(ComponentEvent e) {
-        
-        
+
+
     }
 
     private void write(String data) {
@@ -741,7 +744,7 @@ public class GUIHandler extends JFrame implements ActionListener, ComponentListe
                 }
 
                 return null;
-            }  
+            }
         }.execute();
     }
 
