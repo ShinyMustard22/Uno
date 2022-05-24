16eecdf24b855191d0abe5cae14dde5f18b25b22
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-05-24 11:14:07 -0700

    java docs

--- src/GameState.java
+++ src/GameState.java
@@ -1,6 +1,14 @@
 import java.util.*;
 import cards.*;
 
+/**
+ * A class the represents the state of the game, and is statically updated by
+ * the ClientHandler class. Hold references to the players, deck, and discard
+ * pile as well as other information pretaining to that.
+ * 
+ * @author Angela Chung, Ritam Chakraborty
+ * @version May 24, 2022
+ */
 public class GameState {
     private Queue<Card> deck;
     private Stack<Card> discardPile;
@@ -14,6 +22,9 @@ public class GameState {
     private boolean gameStarted;
     private int currentPlace;
 
+    /**
+     * Constructs a new deck and discard pile. Initializes other fields.
+     */
     public GameState() {
         deck = new Deck();
         discardPile = new Stack<Card>();
@@ -30,6 +41,12 @@ public class GameState {
         currentPlace = 0;
     }
 
+    /**
+     * Adds a player with the provided username.
+     * @param username username of the player to add
+     * @return true if the game hasn't started and if the maximum number of
+     * players hasn't been reached, false otherwise
+     */
     public boolean addPlayer(String username) {
         if (gameStarted || players.size() >= 10) {
             return false;
@@ -46,6 +63,12 @@ public class GameState {
         return true;
     }
 
+    /**
+     * Gets a player with the specified username
+     * @param username username of the player
+     * @return the player with the specified username, null if the username
+     * isn't in the list of players
+     */
     public Player getPlayer(String username) {
         for (Player player : players) {
             if (player.getUsername().equals(username)) {
@@ -56,6 +79,10 @@ public class GameState {
         return null;
     }
 
+    /**
+     * Removes player with a specified username
+     * @param username username of specified player, then fixes the turn system
+     */
     public void removePlayer(String username) {
         for (Player player : players) {
             if (player.getUsername().equals(username)) {
@@ -67,6 +94,11 @@ public class GameState {
         fixIterator();
     }
 
+    /**
+     * Adds player to the players that have already won, removes player from
+     * the game, player determined from specified username.
+     * @param username username of the specified player
+     */
     public void playerWon(String username) {
         currentPlace++;
         Player player = getPlayer(username);
@@ -75,6 +107,11 @@ public class GameState {
         removePlayer(username);
     }
 
+    /**
+     * Gets the place in the game of the player with the sepcified username.
+     * @param username username of the player
+     * @return the place of that player, -1 if the player doesn't exist
+     */
     public int getPlaceOfPlayer(String username) {
         for (Player player : wonPlayers) {
             if (player.getUsername().equals(username)) {
@@ -134,6 +171,10 @@ public class GameState {
         players.add(currentPlayer);
     }
 
+    /**
+     * Advances to the next players turn based on the card that was played.
+     * @param card the card that was played previously
+     */
     public void advanceTurn(Card card) {
         if (currentPlayer.getHandSize() == 0 && players.size() == 2) {
             return;
@@ -209,22 +250,35 @@ public class GameState {
         }
     }
 
-    public boolean startGame() {
+    /**
+     * Starts the game.
+     */
+    public void startGame() {
         gameStarted = true;
         turn = players.listIterator();
         currentPlayer = turn.next();
-
-        return gameStarted;
     }
 
+    /**
+     * Gets the player who's turn is currently going on.
+     * @return player who's turn it is
+     */
     public Player getCurrentPlayer() {
         return currentPlayer;
     }
 
+    /**
+     * Determines whether the game has started or not.
+     * @return true if game has started, false if otherwise
+     */
     public boolean gameHasStarted() {
         return gameStarted;
     }
 
+    /**
+     * Gets the list of players in string format.
+     * @return the list of players in string format
+     */
     public String getPlayerList() {
         ListIterator<Player> iter = players.listIterator();
         String playerList = "";
@@ -242,6 +296,13 @@ public class GameState {
         return discardPile.peek();
     }
 
+    /**
+     * Plays the specified card with the specified index in the players hand,
+     * if they are able to play the card.
+     * @param card card they should play
+     * @param index index of card to play
+     * @return true if the player could play the card, false otherwise
+     */
     public boolean play(Card card, int index) {
         if (currentPlayer.play(discardPile.peek(), index)) {
             discardPile.push(card);
@@ -257,6 +318,11 @@ public class GameState {
         return false;
     }
 
+    /**
+     * Draws a card from the deck if they are allowed to draw.
+     * @return the card that they drew if they were permitted to draw, null if
+     * they were not.
+     */
     public Card draw() {
         if (currentPlayer.addCard(deck.peek(), discardPile.peek())) {
             if (deck.size() == 0) {
@@ -300,6 +366,10 @@ public class GameState {
         return cardsToDraw;
     }
 
+    /**
+     * Gets the amount of players currently in the game.
+     * @return amount of players in the game
+     */
     public int getTotalPlayers() {
         return players.size();
     }
@@ -315,6 +385,10 @@ public class GameState {
         }
     }
 
+    /**
+     * Called when someone says uno. Distributes cards depending on who said it.
+     * @param username username of player who said uno
+     */
     public void saidUno(String username) {
         Player thisPlayer = getPlayer(username);
         for (Player player : unoDangerPlayers) {
@@ -328,6 +402,9 @@ public class GameState {
         unoDangerPlayers.clear();
     }
 
+    /**
+     * Ends the game.
+     */
     public void endGame() {
         Player lastPlayer = players.get(0);
         currentPlace++;
